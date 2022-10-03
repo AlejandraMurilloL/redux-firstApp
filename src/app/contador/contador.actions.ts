d5a4a740adf86cc43ffd0be969f81643b8b1ba3f
DIGITALWARE\AlejandraML <alejandraml@digitalware.com.co>
2022-10-03 10:11:17 -0500

    Se instala ngRx, se crean acciones, reducer y se usan en el app component

---
+++ src/app/contador/contador.actions.ts
@@ -0,0 +1,4 @@
+import { createAction } from '@ngrx/store';
+
+export const incrementar = createAction('[App Component] Incrementar');
+export const decrementar = createAction('[App Component] Decrementar');
