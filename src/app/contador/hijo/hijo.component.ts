af60f9d653e0254f1cfae571b75e05d763684678
DIGITALWARE\AlejandraML <alejandraml@digitalware.com.co>
2022-10-03 11:08:25 -0500

    Se agrega suscripción al contador desde el componente hijo

--- src/app/contador/hijo/hijo.component.ts
+++ src/app/contador/hijo/hijo.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
 
 @Component({
   selector: 'app-hijo',
@@ -8,26 +10,22 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class HijoComponent implements OnInit {
 
-  @Input() contador!: number;
-  @Output() cambioContador = new EventEmitter<number>();
+  contador!: number;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {
+  }
 
   ngOnInit(): void {
+    this.store.select('contador').subscribe(contador => this.contador = contador);
   }
 
   multiplicar() {
-    this.contador *= 2;
-    this.cambioContador.emit(this.contador);
+    // this.contador *= 2;
+    // this.cambioContador.emit(this.contador);
   }
 
   dividir() {
-    this.contador /= 2;
-    this.cambioContador.emit(this.contador);
-  }
-
-  onContadorChanged(contador: any) {
-    this.contador = contador;
-    this.cambioContador.emit(this.contador);
+    // this.contador /= 2;
+    // this.cambioContador.emit(this.contador);
   }
 }
