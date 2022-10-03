7bb74939e66a15a3fc8382ce36baa795dee0df2d
DIGITALWARE\AlejandraML <alejandraml@digitalware.com.co>
2022-10-03 11:00:04 -0500

    Se cambia el switch del reducer por la función createReducer

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,14 +1,27 @@
-import { Action } from "@ngrx/store";
+
+// ********************** Como referencia ******************************
+
+import { createReducer, on } from "@ngrx/store";
 import { decrementar, incrementar } from "./contador.actions";
 
-export function contadorReducer(state: number = 10, action: Action) {
+// export function contadorReducer(state: number = 10, action: Action) {
 
-  switch (action.type) {
-    case incrementar.type:
-      return state + 1;
-    case decrementar.type:
-      return state - 1;
-    default:
-      return state;
-  }
-}
+//   switch (action.type) {
+//     case incrementar.type:
+//       return state + 1;
+//     case decrementar.type:
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// }
+
+// La misma funcionalidad anterior pero con el createReducer de ngRX
+
+export const initialState = 20;
+
+export const contadorReducer = createReducer(
+  initialState,
+  on(incrementar, (state) => state + 1),
+  on(decrementar, (state) => state - 1),
+);
